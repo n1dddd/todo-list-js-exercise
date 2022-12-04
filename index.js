7fe775a0889a2a4bc36dedee999f17dfe2b4aa02
Daniel Seniv <danielseniv@outlook.com>
2022-12-04 21:59:12 +0000

    Object orientated programming todo list

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+function newTask(title,description) {
+  const task = { //object that takes in title of task(variable), description of task, and whether it has been completed or not
+    title: title,
+    description: description,
+    complete: false,
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+    logState: function() { //logs the state of the task, wether it has been completed or not
+      console.log(`${task.title} has ${task.complete ? "" : "not "}been completed`);
+    },
+    markCompleted: function() { //will change the state of completion for the key task.complete in the object true, marking the task completed
+      task.complete = true;
+    },
+  };
+  return task; //return the object and its updated key
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
 
 // DRIVER CODE BELOW
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry","😨");
+const tasks = [task1,task2];
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+task1.logState();
+task1.markCompleted();
+task1.logState();
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+// newTask("Clean Cat Litter"); // task 0
+// newTask("Do Laundry"); // task 1
+
+// logTaskState(0); // Clean Cat Litter has not been completed
+// completeTask(0);
+// logTaskState(0); // Clean Cat Litter has been completed
